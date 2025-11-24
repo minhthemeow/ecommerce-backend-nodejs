38bf41fe81868557878121fa380a5f73785a8779
minhthemeow <mementomoriminh@gmail.com>
2025-11-25 01:35:42 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const compression = require('compression')
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const app = express() 
+
+
+// init middlewares
+//Ghi log request (giám sát & debug):
+app.use(morgan("dev"))
+//Tăng bảo mật (HTTP headers):
+app.use(helmet())
+//Tối ưu hiệu suất (nén dữ liệu): 
+app.use(compression())
+// init database
+
+// init routes
+app.get('/', (req, res, next) => {
+    const strCmprs = "nicetry";
+    return res.status(200).json({
+        message: 'Hello World!',
+        metadata: strCmprs.repeat(10000) 
+    })
+})
+// handling errors
+
+module.exports = app 
